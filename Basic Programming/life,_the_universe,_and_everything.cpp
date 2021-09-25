c649590bc0cca53a68352f7432101d76ee58ef18
Sejal Dahake <sejaldahake2001@gmail.com>
2021-09-26 00:22:53 +0530

    Add solution life, universe and everything

---
+++ Basic Programming/life,_the_universe,_and_everything.cpp
@@ -0,0 +1,24 @@
+// HackerEarth
+// Life, the universe and Everything
+// life,_the_universe,_and_everything.cpp
+// Basic Programming
+// Author: sejalxz
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    while (true)
+    {
+        cin >> i;
+        if (i == 42)
+            break;
+        else
+            cout << i << endl;
+    }
+
+    return 0;
+}
