65bfa96b515cb0de3a699c0230d65e52f217af50
Sejal Dahake <sejaldahake2001@gmail.com>
2021-10-14 22:21:15 +0530

    Add Basic\ Programming/prog.cpp

---
+++ Basic Programming/prog.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+    int t, N, M, i, schools;
+    cin >> t;
+    while (t > 0)
+    {
+        cin >> N >> M;
+        long int A[M];
+
+        for (i = 0; i < M; i++)
+        {
+            cin >> A[i];
+        }
+        sort(A, A + M);
+        i = 0;
+        schools = 0;
+        while (N >= 0)
+        {
+            if ((N - A[i]) >= 0)
+            {
+                N = N - A[i];
+                schools++;
+            }
+            else
+                break;
+            i++;
+        }
+        cout << schools << "\n";
+        t--;
+    }
+    return 0;
+}
