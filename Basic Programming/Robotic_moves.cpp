155d82b7448b40eef46a9782c2c93c11e6bcaacd
Sejal Dahake <sejaldahake2001@gmail.com>
2021-10-16 15:35:48 +0530

    Add solution Robotic Moves- Robotic_moves.cpp

---
+++ Basic Programming/Robotic_moves.cpp
@@ -0,0 +1,21 @@
+// HackerEarth
+// Robotic Moves
+// Basic Programming
+// Author: sejalxz
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    long long N;
+    while (t--)
+    {
+        cin >> N;
+        cout << N * (N + 1) << "\n";
+    }
+    return 0;
+}
